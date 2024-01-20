0b9af70ffedf32a5f2490a3a0d1f7d990ffec092
Cameron Clark <cpclark860@gmail.om>
2024-01-20 08:41:43 -0500

    FailureAutonomous changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FailureAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FailureAutonomous.java
@@ -1,52 +1,81 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(preselectTeleOp = "Failure")
-public class FailureAutonomous extends OpMode {
+public class FailureAutonomous extends LinearOpMode {
 
     public DcMotor frontLeft, frontRight, rearLeft, rearRight;
     public boolean finished = false;
-    public double timeToRun = 10;
+    public double timeToRunShort = 4.85 / 2;
+    public double timeToRunLong = 4.85;
+    public boolean runLong;
     private ElapsedTime runtime = new ElapsedTime();
-    @Override
-    public void init()
+    private Gamepad prevPad1 = new Gamepad();
+    public void runOpMode()
     {
-        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
-        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
-        rearLeft = hardwareMap.get(DcMotor.class, "rearLeft");
-        rearRight = hardwareMap.get(DcMotor.class, "rearRight");
-    }
+        frontRight = hardwareMap.get(DcMotor.class, "frontRightMotor");
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeftMotor");
+        rearLeft = hardwareMap.get(DcMotor.class, "rearLeftMotor");
+        rearRight = hardwareMap.get(DcMotor.class, "rearRightMotor");
 
-    @Override
-    public void start() {
+        frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        rearLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        rearRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        boolean confirmed = false;
+        while (!confirmed)
+        {
+            if (gamepad1.right_bumper)
+                runLong = true;
+            else if (gamepad1.left_bumper)
+                runLong = false;
+
+            if (gamepad1.start && gamepad1.a && !prevPad1.a)
+                confirmed = true;
+
+            telemetry.addData("Travel Long? ", runLong);
+            telemetry.addData("Confirmed? ", confirmed);
+            telemetry.update();
+
+            prevPad1.copy(gamepad1);
+        }
+
+        telemetry.addLine("Waiting...");
+        telemetry.update();
+
+        waitForStart();
+        // start
         frontRight.setPower(1);
         frontLeft.setPower(1);
         rearLeft.setPower(1);
         rearRight.setPower(1);
+
         runtime.reset();
-    }
-
-
-    @Override
-    public void loop() {
-        if (!finished)
+        while (opModeIsActive())
         {
-            double time = runtime.seconds();
-            if (time == timeToRun)
+            if (!finished)
             {
-                finished = true;
+                double time = runtime.seconds();
+                if (time >= ((runLong) ? timeToRunLong : timeToRunShort))
+                {
+                    finished = true;
 
-                frontRight.setPower(0);
-                frontLeft.setPower(0);
-                rearRight.setPower(0);
-                rearLeft.setPower(0);
+                    frontRight.setPower(0);
+                    frontLeft.setPower(0);
+                    rearRight.setPower(0);
+                    rearLeft.setPower(0);
+                }
             }
-        }
 
-        telemetry.addData("Elapsed Time: ", runtime.seconds());
+            telemetry.addData("Elapsed Time: ", runtime.seconds());
+        }
     }
 }
