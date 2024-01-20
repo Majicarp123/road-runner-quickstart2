d5394e2542ee6445ef2f96375eb724d7f681a838
Majicarp123 <Riley.Kennedy@wordtotheboro.org>
2024-01-19 23:44:15 -0500

    AAAAAAAAAAAA

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoE.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoE.java
@@ -1,92 +1,109 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Gamepad;
 
-@Autonomous(preselectTeleOp = "TeleOpE")
-public class AutoE extends LinearOpMode{
+import org.openftc.easyopencv.OpenCvWebcam;
 
-    Boolean allianceRed = null;
-    Boolean startLeft = null;
-    Boolean pathDirect = null;
-    Boolean parkingLeft = null;
-    boolean initialized = false;
+enum Route {
+    RED_LEFT_DIRECT,
+    RED_LEFT_INDIRECT,
+    RED_RIGHT_DIRECT,
+    RED_RIGHT_INDIRECT,
+    BLUE_LEFT_DIRECT,
+    BLUE_LEFT_INDIRECT,
+    BLUE_RIGHT_DIRECT,
+    BLUE_RIGHT_INDIRECT,
+    PARK_LEFT,
+    PARK_RIGHT
+
+}
+
+@Autonomous(preselectTeleOp = "TeleOpA")
+//@Disabled
+public class AutoE extends LinearOpMode {
+
+
+    public static final Vector2d RED_LEFT_START = new Vector2d(-36, -61);
+    public static final Vector2d RED_RIGHT_START = new Vector2d(12, -61);
+    public static final Vector2d RED_RIGHT_LEFT_POSITION = new Vector2d(14, -30);
+    public static final Vector2d RED_RIGHT_RIGHT_POSITION = new Vector2d(23, -30);
+
+
+    public static final Vector2d BLUE_LEFT_START = new Vector2d(12, 61);
+    public static final Vector2d BLUE_RIGHT_START = new Vector2d(-36, 61);
+    public static final Vector2d BLUE_RIGHT_LEFT_POSITION = new Vector2d(23, 30);
+    public static final Vector2d BLUE_RIGHT_RIGHT_POSITION = new Vector2d(14, 30);
+
+    private Boolean redAlliance = null;
+    private Boolean startLeft = null;
+    private Boolean parkLeft = null;
+
+    OpenCvWebcam camera;
+    private boolean startedStreaming;
+
+
+    public static final Route ROUTE = Route.RED_LEFT_DIRECT;
+    public static final double DELAY = 0.5;
+
     @Override
-    public void runOpMode(){
+    public void runOpMode() {
+        Gamepad previousGamepad = new Gamepad();
+        Gamepad currentGamepad = new Gamepad();
 
+        while (true) {
+            previousGamepad.copy(currentGamepad);
+            currentGamepad.copy(gamepad1);
 
-        boolean previousX = false;
-        boolean previousB = false;
-
-        while (!initialized)  {
-
-            boolean currentX = gamepad1.x;
-            boolean currentB = gamepad1.b;
-
-            if (allianceRed== null) {
+            if (redAlliance == null) {
                 telemetry.addData("Alliance", "X = blue, B = red");
                 telemetry.update();
-                if (currentX && !previousX) {
-                    allianceRed = false;
+                if (currentGamepad.x && !previousGamepad.x) {
+                    redAlliance = false;
                 }
-                if (currentB && !previousB) {
-                    allianceRed = true;
+                if (currentGamepad.b && !previousGamepad.b) {
+                    redAlliance = true;
                 }
-            }
-            else if (startLeft == null) {
+            } else if (startLeft == null) {
                 telemetry.addData("Start", "X = left, B = right");
                 telemetry.update();
-                if (currentX && !previousX) {
-                     startLeft = true;
+                if (currentGamepad.x && !previousGamepad.x) {
+                    startLeft = true;
                 }
-                if (currentB && ! previousB) {
+                if (currentGamepad.b && !previousGamepad.b) {
                     startLeft = false;
                 }
-            } else if (pathDirect == null) {
-                telemetry.addData("Path", "X = direct, B= indirect");
+            } else if (parkLeft == null) {
+                telemetry.addData("Park", "X = left, B = right");
                 telemetry.update();
-                if (currentX && !previousX) {
-                    pathDirect = true;
+                if (currentGamepad.x && !previousGamepad.x) {
+                    parkLeft = true;
                 }
-                if (currentB && !previousB) {
-                    pathDirect = false;
+                if (currentGamepad.b && !previousGamepad.b) {
+                    parkLeft = false;
                 }
-            } else if (parkingLeft == null) {
-                telemetry.addData("Parking", "X = left, B = right");
-                telemetry.update();
-                if (currentX && !previousX) {
-                    parkingLeft = true;
-                }
-                if (currentB && !previousB) {
-                    parkingLeft = false;
-                }
-            }
-            else {
-                initialized = true;
-            }
-            previousX = currentX;
-            previousB = currentB;
-        }
-
-        telemetry.addData("Status", "Initialized");
-        telemetry.addData("Alliance", allianceRed ? "Red" : "Blue");
-        telemetry.addData("Start", startLeft ? "Left" : "Right");
-        telemetry.addData("Path", pathDirect ? "Direct" : "Indirect");
-        telemetry.addData("Parking", parkingLeft ? "Left" : "Right");
-        telemetry.update();
-
-        waitForStart();
-        while (opModeIsActive()){
-            telemetry.addData("Status", "Running");
-            telemetry.update();
-                }
+            } else {
+                break;
             }
         }
 
 
+    }
 
 
+    //red left park left
+    //red left park right
+    //red right park right
+    //red right park left
 
 
+// Blue
+//blue left park right
+    //blue left park left
+    //blue right park right
+    //blue right park left
+
+}
 
